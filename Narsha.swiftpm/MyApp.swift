f4779677d9d539bfce09c5bcef4c0c309d7b83ec
suri0000 <yesle2005@naver.com>
2024-02-21 21:52:51 +0900

    [Feat/#1] Font 적용

--- Narsha.swiftpm/MyApp.swift
+++ Narsha.swiftpm/MyApp.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 @main
 struct MyApp: App {
+  
+  init() {
+    FontManager.registerFonts()
+  }
+  
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
--- Narsha.swiftpm/View/MainView.swift
+++ Narsha.swiftpm/View/MainView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Narsha")
+          .font(.custom("KoddiUDOnGothic-Bold", size: 36))
+          .bold()
     }
 }
 
