ac300b9d1f8e66392613eb5ca144a9b9d44a0ce4
suri0000 <yesle2005@naver.com>
2024-02-26 01:04:51 +0900

    [Feat/#21] TabIconView로 이름 변경

--- Narsha.swiftpm/LearnIconsScene/View/TabIconView.swift
+++ Narsha.swiftpm/LearnIconsScene/View/TabIconView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct MirroringView: View {
+struct TabIconView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
     }
 }
 
 #Preview {
-    MirroringView()
+    TabIconView()
 }
