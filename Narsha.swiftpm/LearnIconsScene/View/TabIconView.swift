6063b186082455ca6d79da1f72eb319e2cc1c892
suri0000 <yesle2005@naver.com>
2024-02-26 01:41:16 +0900

    [Feat/#21] TabIconView UI

--- Narsha.swiftpm/LearnIconsScene/View/TabIconView.swift
+++ Narsha.swiftpm/LearnIconsScene/View/TabIconView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct TabIconView: View {
+  
+  
     var body: some View {
-        
+      NavigationStack {
+        VStack {
+          Directive(directive: "Gather the purple squares to where the gray squares are.")
+          
+          HStack {
+            Rectangle()
+              .frame(width: 103, height: 102)
+              .foregroundColor(Color(hex: 0xE4DFFF))
+              .border(.black, width: 10)
+              .cornerRadius(15)
+            
+            VStack {
+              Rectangle()
+                .frame(width: 103, height: 102)
+                .foregroundColor(Color(hex: 0xE4DFFF))
+                .border(.black, width: 10)
+                .cornerRadius(15)
+              Rectangle()
+                .frame(width: 103, height: 102)
+                .foregroundColor(Color(hex: 0xE4DFFF))
+                .border(.black, width: 10)
+                .cornerRadius(15)
+            }
+          }
+          .padding(.bottom, 70)
+          
+          Rectangle()
+            .frame(width: 103, height: 102)
+            .foregroundColor(.white)
+            .border(Color(hex: 0xE5E5E5), width: 10)
+            .cornerRadius(15)
+        }
+      }
     }
 }
 
