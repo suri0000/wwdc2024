29a4f4d84a25612ced35fca25867c3c6af2f08ee
suri0000 <yesle2005@naver.com>
2024-02-26 00:38:18 +0900

    [Feat/#19] RefreshView 완성

--- Narsha.swiftpm/LearnIconsScene/View/RefreshView.swift
+++ Narsha.swiftpm/LearnIconsScene/View/RefreshView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct RefreshView: View {
+  
+  @State private var angle: Angle = .init(degrees: 20)
+  @State var rotated = false
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      NavigationStack {
+        VStack {
+          Directive(directive: "Rotate it clockwise using two fingers")
+          
+          Image(systemName: "arrow.clockwise")
+            .resizable()
+            .scaledToFit()
+            .frame(width: UIWidth * 0.25, height: UIHeight * 0.13)
+            .rotationEffect(angle)
+            .gesture(rotationGesture)
+            .padding(.vertical, 205)
+          
+          if rotated {
+            NavigationLink {
+              IconDescriptionView(iconName: Description.refresh.rawValue, destination: MirroringView())
+            } label: {
+              NextButton()
+            }
+          }
+        }
+      }
     }
+  
+  private var rotationGesture: some Gesture {
+    RotationGesture()
+      .onChanged {
+        print($0)
+        angle = $0
+        rotated = true
+      }
+  }
 }
 
 #Preview {
